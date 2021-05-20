33ad100aeea62f5bc4f4f8b64a724467807ead8f
michail vazaios <mich.vaz.99@gmail.com>
2021-05-20 18:14:43 +0300

    Commit everything

--- app/src/main/java/com/example/hciproject1/OnStateItemClickListener.java
+++
@@ -1,9 +0,0 @@
-package com.example.hciproject1;
-
-import com.example.hciproject1.components.StateItem;
-
-public interface OnStateItemClickListener {
-
-    void onStateItemClick(StateProgressBar stateProgressBar, StateItem stateItem, int stateNumber, boolean isCurrentState);
-
-}
